93f9a87981e3d6e560add8f69f0067712a051011
Matthew Knox <matthew@makereti.co.nz>
2017-05-08 12:31:06 +1200

    Initial commit

---
+++ firstrun.js
@@ -0,0 +1,101 @@
+const fs = require('fs-extra'),
+    request = require('request'),
+    path = require('path'),
+    git = require('concierge/git');
+
+class FirstRun {
+    _getJsonFile (file) {
+        try {
+            return this._getJson(fs.readFileSync(file, 'utf8'));
+        }
+        catch (e) {
+            return null;
+        }
+    }
+
+    _getJson (data) {
+        try {
+            return JSON.parse(data.replace(/^\uFEFF/, ''));
+        }
+        catch (e) {
+            return null;
+        }
+    }
+
+    _getRemoteDefaults () {
+        return new Promise(resolve => {
+            const potentialDefaultUrls = [
+                platform.config.getSystemConfig('defaults').url || null,
+                this.config.url || null,
+                this._getJson(process.env.CONCIERGE_DEFAULTS_URL),
+                'https://raw.githubusercontent.com/wiki/concierge/Concierge/Defaults.md'
+            ];
+            const url = potentialDefaultUrls.find(p => !!p);
+            request(url, (err, response, body) => {
+                if (err) {
+                    resolve(null);
+                }
+                else {
+                    const tableArea = body.substring(body.indexOf('***') + 3, body.lastIndexOf('***')),
+                        relevantArea = tableArea.substring(tableArea.indexOf('--|\n') + 4),
+                        defaults = relevantArea.split(/\r?\n/)
+                            .map(r => r.split('|').map(s => s.trim()).filter(s => !!s)
+                            .slice(0, 2).reverse()).filter(r => r.length === 2);
+                    resolve(defaults);
+                }
+            });
+        });
+    }
+
+    _installModule (def) {
+        return new Promise(resolve => {
+            console.warn($$`Attempting to install module from "${def[0]}"`);
+            const installPath = path.join(global.__modulesPath, def[1]);
+            git.clone(def[0], installPath, err => {
+                if (err) {
+                    console.critical(err);
+                    console.error($$`Failed to install module from "${def[0]}"`);
+                }
+                else {
+                    console.warn($$`"${def[1]}" (${this._getJsonFile(path.join(installPath, 'kassy.json')).version}) is now installed.`);
+                }
+                resolve(true);
+            });
+        });
+    }
+
+    load (platform) {
+        process.nextTick(() => { // let startup complete
+            if (platform.modulesLoader.getLoadedModules().length > 1) {
+                LOG.error('Modules are already installed, will not continue.');
+                return;
+            }
+            this._getRemoteDefaults().then(remoteDefaults => {
+                const potentialDefaults = [
+                    this._getJsonFile(global.rootPathJoin('defaults.json')),
+                    platform.config.getSystemConfig('defaults').list || null,
+                    this.config.list || null,
+                    this._getJson(process.env.CONCIERGE_DEFAULTS),
+                    remoteDefaults
+                ];
+                const defaultModules = potentialDefaults.find(p => !!p);
+                if (!defaultModules) {
+                    LOG.error('No defaults list is available to install.');
+                    return;
+                }
+                const installPromises = [];
+                for (let def of defaultModules) {
+                    installPromises.push(this._installModule(def));
+                }
+                Promise.all(installPromises).then(() => {
+                    platform.modulesLoader.loadAllModules();
+                    const dir = this.__descriptor.folderPath;
+                    platform.modulesLoader.unloadModule(this.__descriptor);
+                    fs.remove(dir, () => {});
+                });
+            });
+        });
+    }
+}
+
+module.exports = new FirstRun();
