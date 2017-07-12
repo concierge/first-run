1309a9a70d95c5c224c55c7587a9919d473a887b
Matthew Knox <mrkno@users.noreply.github.com>
2017-07-12 21:29:20 +1200

    Async module loading (#1)
    * Implement async installation of default modules
    See upstream:#248

--- firstrun.js
+++ firstrun.js
@@ -12,16 +12,16 @@
  *              Copyright (c) Matthew Knox and Contributors 2017.
  */
 
-const fs = require('fs'),
+const path = require('path'),
+    util = require('util'),
     client = require('scoped-http-client'),
-    path = require('path'),
     git = require('concierge/git'),
     files = require('concierge/files');
 
 class FirstRun {
-    _getJsonFile (file) {
+    async _getJsonFile (file) {
         try {
-            return this._getJson(fs.readFileSync(file, 'utf8'));
+            return await files.readJson(file);
         }
         catch (e) {
             return null;
@@ -37,35 +37,33 @@ class FirstRun {
         }
     }
 
-    _getRemoteDefaults (globalConfig) {
-        return new Promise(resolve => {
-            const potentialDefaultUrls = [
-                globalConfig.url || null,
-                this.config.url || null,
-                this._getJson(process.env.CONCIERGE_DEFAULTS_URL),
-                'https://raw.githubusercontent.com/wiki/concierge/Concierge/Defaults.md'
-            ];
-            const url = potentialDefaultUrls.find(p => !!p);
-            client.create(url).get()((err, response, body) => {
-                if (err) {
-                    resolve(null);
-                }
-                else {
-                    const tableArea = body.substring(body.indexOf('***') + 3, body.lastIndexOf('***')),
-                        relevantArea = tableArea.substring(tableArea.indexOf('--|\n') + 4),
-                        defaults = relevantArea.split(/\r?\n/)
-                            .map(r => r.split('|').map(s => s.trim()).filter(s => !!s)
-                            .slice(0, 2).reverse()).filter(r => r.length === 2);
-                    resolve(defaults);
-                }
-            });
-        });
+    async _getRemoteDefaults (globalConfig) {
+        const potentialDefaultUrls = [
+            globalConfig.url || null,
+            this.config.url || null,
+            this._getJson(process.env.CONCIERGE_DEFAULTS_URL),
+            'https://raw.githubusercontent.com/wiki/concierge/Concierge/Defaults.md'
+        ];
+        const url = potentialDefaultUrls.find(p => !!p);
+        try {
+            const response = await util.promisify((u, c) => client.create(u).get()((e, r, b) => c(e, b)))(url);
+            const tableArea = response.substring(response.indexOf('***') + 3, response.lastIndexOf('***')),
+                relevantArea = tableArea.substring(tableArea.indexOf('--|\n') + 4),
+                defaults = relevantArea.split(/\r?\n/)
+                .map(r => r.split('|').map(s => s.trim()).filter(s => !!s)
+                .slice(0, 2).reverse()).filter(r => r.length === 2);
+            return defaults;
+        }
+        catch (e) {
+            LOG.critical(e);
+            return null;
+        }
     }
 
-    _getInstalledVersion (installPath) {
-        const descriptor = this._getJsonFile(path.join(installPath, 'kassy.json')) ||
-            this._getJsonFile(path.join(installPath, 'hubot.json')) ||
-            this._getJsonFile(path.join(installPath, 'package.json'));
+    async _getInstalledVersion (installPath) {
+        const descriptor = await this._getJsonFile(path.join(installPath, 'kassy.json')) ||
+            await this._getJsonFile(path.join(installPath, 'hubot.json')) ||
+            await this._getJsonFile(path.join(installPath, 'package.json'));
         if (typeof(descriptor.version) === 'string') {
             return descriptor.version;
         }
@@ -74,53 +72,41 @@ class FirstRun {
         return spl.concat(Array(3 - spl.length).fill('0')).join('.');
     }
 
-    _installModule (def) {
-        return new Promise(resolve => {
-            LOG.warn($$`Attempting to install module from "${def[0]}"`);
-            const installPath = path.join(global.__modulesPath, def[1]);
-            git.clone(def[0], installPath, err => {
-                if (err) {
-                    LOG.critical(err);
-                    LOG.error($$`Failed to install module from "${def[0]}"`);
-                }
-                else {
-                    LOG.warn($$`"${def[1]}" (${this._getInstalledVersion(installPath)}) is now installed.`);
-                }
-                resolve(true);
-            });
-        });
+    async _installModule (def) {
+        LOG.warn($$`Attempting to install module from "${def[0]}"`);
+        const installPath = path.join(global.__modulesPath, def[1]);
+        try {
+            await git.clone(def[0], installPath);
+            const installedVersion = await this._getInstalledVersion(installPath);
+            LOG.warn($$`"${def[1]}" (${installedVersion}) is now installed.`);
+        }
+        catch (err) {
+            LOG.critical(err);
+            LOG.error($$`Failed to install module from "${def[0]}"`);
+        }
+        return true;
     }
 
-    load (platform) {
+    async load (platform) {
         const globalConfig = platform.config.getSystemConfig('defaults');
-        this._getRemoteDefaults(globalConfig).then(remoteDefaults => {
-            const potentialDefaults = [
-                this._getJsonFile(global.rootPathJoin('defaults.json')),
-                globalConfig.list || null,
-                this.config.list || null,
-                this._getJson(process.env.CONCIERGE_DEFAULTS),
-                remoteDefaults
-            ];
-            const defaultModules = potentialDefaults.find(p => !!p);
-            if (!defaultModules) {
-                LOG.error('No defaults list is available to install.');
-                return;
-            }
-            const installPromises = [];
-            for (let def of defaultModules) {
-                installPromises.push(this._installModule(def));
-            }
-            Promise.all(installPromises).then(() => {
-                platform.modulesLoader.unloadModule(this);
-            });
-        });
+        const potentialDefaults = [
+            await this._getJsonFile(global.rootPathJoin('defaults.json')),
+            globalConfig.list || null,
+            this.config.list || null,
+            this._getJson(process.env.CONCIERGE_DEFAULTS),
+            await this._getRemoteDefaults(globalConfig)
+        ];
+        const defaultModules = potentialDefaults.find(p => !!p);
+        if (!defaultModules) {
+            return LOG.error('No defaults list is available to install.');
+        }
+        await Promise.all(defaultModules.map(def => this._installModule(def)));
+        await files.deleteDirectory(this.__descriptor.folderPath);
+        platform.modulesLoader.unloadModule(this);
     }
 
-    unload (platform) {
-        const dir = this.__descriptor.folderPath;
-        platform.modulesLoader.once('unload', () => {
-            files.deleteDirectory(dir, () => platform.modulesLoader.loadAllModules());
-        });
+    async unload (platform) {
+        await platform.modulesLoader.loadAllModules();
     }
 }
 
